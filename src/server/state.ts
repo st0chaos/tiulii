7294bc92733d27afd28ed7a5c7cb35959e2ebf5f
st0chaos <st0chaos@posteo.com>
2026-08-27 17:48:40 +0800

    refactor(state): implement FullSyncDocument

--- src/server/state.ts
+++ src/server/state.ts
@@ -21,18 +21,46 @@ import { type Parser } from "./shared.js";
 import { TextDocument } from "vscode-languageserver-textdocument";
 import assert from "node:assert";
 
+class FullSyncDocument {
+  private version: number = 0;
+  private document: TextDocument;
+  private changes$: Subject<TextDocumentContentChangeEvent[]> = new Subject();
+
+  constructor(
+    uri: string,
+    languageId: string,
+    content: string,
+    handler: (content: string, version: number) => void,
+  ) {
+    this.document = TextDocument.create(uri, languageId, this.version, content);
+    this.changes$
+      .pipe(
+        bufferWhen(() => {
+          const size = new TextEncoder().encode(this.document.getText()).length;
+          if (size < 1024 * 1024 * 8) return timer(20);
+          return timer((size / 1024 ** 2) * 30);
+        }),
+      )
+      .subscribe(async (events) => {
+        const changes = events.flat();
+        if (changes.length === 0) return;
+        this.version++;
+        TextDocument.update(this.document, changes, this.version);
+        handler(this.document.getText(), this.version);
+      });
+  }
+  destroy() {
+    this.changes$.complete();
+  }
+  update(change: TextDocumentContentChangeEvent[]) {
+    this.changes$.next(change);
+  }
+}
+
 interface Document {
   parser: Parser;
   html: string;
-  fullSync?: FullSyncData;
-}
-interface FullSyncData {
-  document: TextDocument;
-  changeEvent$: Subject<ChangeEvent>;
-  subscription: Subscription;
-}
-interface ChangeEvent {
-  changes: TextDocumentContentChangeEvent[];
+  fullSync?: FullSyncDocument;
 }
 
 class State {
@@ -77,37 +105,23 @@ class State {
       return;
     }
 
-    let ver = 0;
-    const document = TextDocument.create(uri, language, ver, text);
-    const changeEvent$ = new Subject<ChangeEvent>();
-    const subscription = changeEvent$
-      .pipe(
-        bufferWhen(() => {
-          const size = new TextEncoder().encode(document.getText()).length;
-          if (size < 1024 * 1024 * 8) return timer(20);
-          return timer((size / 1024 ** 2) * 25);
-        }),
-      )
-      .subscribe(async (events) => {
-        const changes = events.map((ev) => ev.changes).flat();
-        if (changes.length === 0) return;
-        ver++;
-        TextDocument.update(document, changes, ver);
-        this.documents$.next({
-          ...this.documents$.value,
-          [uri]: {
-            ...this.documents$.value[uri]!,
-            html: await parser.parse(document.getText(), uri, ver),
-          },
-        });
+    const fullSync = new FullSyncDocument(uri, language, text, async (text) => {
+      const value = this.documents$.value;
+      this.documents$.next({
+        ...value,
+        [uri]: {
+          ...value[uri]!,
+          html: await parser.parse(text, uri, version),
+        },
       });
+    });
 
     this.documents$.next({
       ...this.documents$.value,
       [uri]: {
         parser,
-        html: await parser.parse(text, uri, ver),
-        fullSync: { changeEvent$, document, subscription },
+        fullSync,
+        html: await parser.parse(text, uri, 0),
       },
     });
   }
@@ -130,7 +144,7 @@ class State {
       });
     } else {
       assert(fullSync);
-      fullSync.changeEvent$.next({ changes });
+      fullSync.update(changes);
     }
   }
 
@@ -145,7 +159,7 @@ class State {
 
     const document = this.documents$.value[uri];
     if (document === undefined) return;
-    document.fullSync?.subscription.unsubscribe();
+    document.fullSync?.destroy();
     document.parser.close?.(uri);
 
     const { [uri]: _, ...rest } = this.documents$.value;
